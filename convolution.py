8b7b49fadf1bb92418b330e990c1339b3b194e22
Sophia <71236168+the-soph-ia@users.noreply.github.com>
2021-12-18 20:53:03 -0600

    Updated to include any size kernel

--- convolution.py
+++ convolution.py
@@ -15,8 +15,8 @@ new_img = np.zeros([mat1.shape[0], mat1.shape[1]])
 
 for y in range(new_img.shape[0]-kernel.shape[0]+1):
     for x in range(new_img.shape[1]-kernel.shape[1]+1):
-        new_img[y+1][x+1] = np.sum((kernel)*np.asarray([[mat1[y][x],mat1[y][x+1],mat1[y][x+2]],[mat1[y+1][x],mat1[y+1][x+1],mat1[y+1][x+2]],[mat1[y+2][x],mat1[y+2][x+1],mat1[y+1][x+2]]]))
-
+        new_img[y+1][x+1] = np.sum((kernel)*np.asarray([mat1[y][x:x+kernel.shape[1]] for y in range(y, y+kernel.shape[0])]))
+        
 print(np.asarray(new_img//1))
 
 filtered = Image.fromarray(new_img)
